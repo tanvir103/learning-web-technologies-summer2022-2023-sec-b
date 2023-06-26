25976b41c12f9d96669e923d01aa2b2317a4889c
Tanvir Hasan Tamal <96726109+tanvir103@users.noreply.github.com>
2023-06-27 04:52:50 +0600

    Update add_product .php

--- Lab Task 6/add_product .php
+++ Lab Task 6/add_product .php
@@ -1,19 +1,15 @@
- <?php
-    if(!isset($_POST['save'])) header('location: AddProduct.php');
+<?php
+    if(!isset($_POST['save'])); 
     else{
         $name = $_POST['name'];
         $buyingprice = $_POST['buyingPrice'];
         $sellingprice = $_POST['sellingPrice'];
-        
-        if(isset($_POST['display'])) $display = 'true';
-        else $display = 'false';
-
-
+       
 
         $conn = mysqli_connect('localhost', 'root', '', 'product_db');
-        $sql = "INSERT INTO products VALUES('$name', '$buyingprice', '$sellingprice', '$display');";
+        $sql = "INSERT INTO products VALUES('$name', '$buyingprice', '$sellingprice');";
 
         mysqli_query($conn, $sql);
-        header('location: DisplayProduct.php');
+       
     }
 ?>
